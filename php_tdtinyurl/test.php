a9e9f49e0d82d42b2098c66a13cc6d40fd339623
Torden <ioemen@gmail.com>
2013-12-09 01:45:29 +0900

    bug fixed

--- php_tdtinyurl/test.php
+++ php_tdtinyurl/test.php
@@ -1,7 +1,7 @@
 #!/usr/local/php-fpm/bin/php
 <?php
 
-$sUrl = 'http://www.yahoo.com';
+$sUrl = 'http://www.yahoo.com/'.time().'/'.uniqid();
 
 print "\n\n";
 $sKey = tdtinyurl_set_url($sUrl, TDTINYURL_MODE_SECUER_TUNNELING);
@@ -15,8 +15,6 @@ print 'tdtinyurl_set_rid : '. $mRet."\n";
 
 $sRid = tdtinyurl_get_rid($sKey);
 print 'tdtinyurl_get_rid : '. $sRid ."\n";
-
-
 
 
 print "\n\n";
